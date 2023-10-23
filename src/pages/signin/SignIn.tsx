502d7a0d4e3021f254c4496ff8d727a14651ca5f
Martin Haas <martinhaas2005@gmail.com>
2023-10-23 17:08:19 -0300

    feat: created signin page

--- src/pages/signin/SignIn.tsx
+++ src/pages/signin/SignIn.tsx
@@ -1,10 +1,34 @@
+import { useState } from 'react';
+import AuthBackground from '../../components/authBackground/AuthBackground';
+import Input from '../../components/input/Input';
 import './signin.scss';
 
 const SignIn = () => {
+
+  const queryValues: string[] = ['name', 'age', 'email', 'password'];
+  const [queryIndex, setQueryIndex] = useState(0);
+
+  const handleClick = (e: React.FormEvent<HTMLElement>) => {
+    e.preventDefault();
+    setQueryIndex(n => n + 1);
+  };
+
+  const queryValue = queryValues[queryIndex];
+
   return(
-    <div className='signIn'>
-      sign In
-    </div>
+    <AuthBackground>
+      <div className="signIn">
+        <img src="logo.svg" alt="TypeTask Pro Logo" className='logo'/>
+        <div className="form-container">
+          <form>
+            <Input id={queryValue} placeholder={`enter your ${queryValue}`} label={queryValue} className='singIn-input'/>
+            <button onClick={handleClick}>
+              clica
+            </button>
+          </form>
+        </div>
+      </div>
+    </AuthBackground>
   )
 }
 
