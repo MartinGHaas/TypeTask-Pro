cf99dc29983e788379a5f8f7dc0d813ae26cc083
Martin Haas <martinhaas2005@gmail.com>
2023-11-02 00:51:50 -0300

    docs: added UserContext docs

--- src/context/UserContext.tsx
+++ src/context/UserContext.tsx
@@ -9,26 +9,35 @@ export interface IUserContext {
   setUser: Dispatch<SetStateAction<User>>
 }
 
+/** Defines user default state. */
 const defaultState = {
   user: {
     isLogged: false
   },
-  setUser: () => {}
+  setUser: () => { }
 } as IUserContext;
 
+/** Provides User Context for other Components to use. */
 export const UserContext = createContext(defaultState);
 
 type UserProviderProps = {
+  /** Children Elements. */
   children: ReactNode;
 };
 
-export const UserProvider = ({ children }: UserProviderProps) => {
+/**
+ * Create User Context Provider
+ * @param {UserProviderProps} props Component Props.
+ * @param {ReactNode} props.children Children Elements.
+ * @returns {JSX.Element} provide user state for inner Components.
+ */
+export const UserProvider = ({ children }: UserProviderProps): JSX.Element => {
   const [user, setUser] = useState<User>({
     isLogged: false
   });
 
   return (
-    <UserContext.Provider value={{user, setUser}}>
+    <UserContext.Provider value={{ user, setUser }}>
       {children}
     </UserContext.Provider>
   )
