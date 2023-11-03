fca96ee287d3868ee371af33dfbdf64978f28380
Martin Haas <martinhaas2005@gmail.com>
2023-11-03 00:54:49 -0300

    docs: created Button docs

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -2,14 +2,38 @@ import { ReactNode } from 'react';
 import './button.scss';
 
 type ButtonProps = {
-  children: ReactNode;
+  /**
+   * Changes the button UI background from transparent
+   * to a colorful one.
+   * @default false
+   */
   isFilled?: boolean;
+
+  /**
+   * Animates the button hover w/ a default
+   * left to right animation.
+   * @default false
+   */
   animated?: boolean;
+
+  children: ReactNode;
+
   handleClick?: (e: React.FormEvent<HTMLElement>) => void;
+
   className?: string;
 }
 
-const Button = ({ children, isFilled, handleClick, className, animated }: ButtonProps) => {
+/**
+ * Reusable Button Component
+ * @param {ButtonProps} props Button Component props
+ * @param {ReactNode} props.children - Buttons content.
+ * @param {boolean} [props.isFilled] - Changes button background.
+ * @param {boolean} [props.animated=false] - Animates button hover.
+ * @param {(e: React.FormEvent<HTMLElement>) => void} [props.handleClick] - Callback handleClick function.
+ * @param {string} [props.className] - CSS Class to button container div.
+ * @returns {JSX.Element} Reusable Button Component
+ */
+const Button = ({ children, isFilled, handleClick, className, animated }: ButtonProps): JSX.Element => {
   return (
     <div className={`button-container ${className ? className : ''} `}>
       <button onClick={handleClick} className={`${isFilled ? 'isFilled' : ''} ${animated ? 'animated' : ''}`}>
